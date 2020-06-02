22b546f8cce098b4daaac299d63bf82936dfce62
EMQ-YangM <yangm@emqx.io>
2020-06-02 15:29:20 +0800

    fix(Utils.hs,Parser.y): fix the bug that ':' does not work

--- tests/Test.purs
+++ tests/Test.purs
@@ -1,18 +1,11 @@
 module Test where
 
--- foreign import error :: forall a. String -> a
--- foreign import showAny :: forall a.  a -> String
-
--- t1 :: [Integer] -> [Integer]
--- t1 [a,b,c|t] = t
--- t1 _ = [1,2,3]
-
--- t2 x = let [a,b,c|t] = x
---        in (a,b,c,t)
-
-
 infixr 6 t as :
 
 t :: Integer -> Integer -> Integer
 t a b = a
 
+
+t1 = 1 : 2 : 3 : 4 : 5 : 6
+-- infixr 6 t as : var t1 = t(1)(t(2)(t(3)(t(4)(t(5)(6)))));
+-- infixl 6 t as : var t1 = t(t(t(t(t(1)(2))(3))(4))(5))(6);
