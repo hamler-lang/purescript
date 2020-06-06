946ecdf1f22e15648a96a4c21b744ed50a735342
EMQ-YangM <yangm@emqx.io>
2020-06-06 13:41:02 +0800

    fix(Exhaustive): fix the completeness check when tuple elements match

--- tests/Test.purs
+++ tests/Test.purs
@@ -1,4 +1,19 @@
 module Test where
 
+data T = T | N | M | Q
 
-t1 = [:a|[:a,:b,:c|[:adfsa,:e]]]
+
+t :: (T,T) -> T
+t (T,_) = T
+t (M,_) = N
+t (x,N) = N
+t (y,Q) = y
+t (N,x) = N
+t (Q,x) = N
+
+j :: List T -> List T
+j [] = []
+j [T|y] = []
+j [N|y] = []
+j [M|y] = []
+j [Q|y] = []
