3ab4be95d51e257e43056a369049094d4b2b6446
Yang Miao <63328943+EMQ-YangM@users.noreply.github.com>
2020-06-09 13:04:56 +0800

    feat(Exhaustive): more tuple completeness checks (#38)

--- src/Language/PureScript/AST/Literals.hs
+++ src/Language/PureScript/AST/Literals.hs
@@ -44,4 +44,21 @@ data Literal a
   | TupleLiteral6 a a a a a a
   | TupleLiteral7 a a a a a a a
 
-  deriving (Eq, Ord, Show, Functor)
+  deriving (Eq, Ord, Functor)
+instance Show a => Show (Literal a) where
+  show (BooleanLiteral t) = "Boolean " <> show t
+  show (NumericLiteral (Left i)) = "Integer " <> show i
+  show (NumericLiteral (Right i)) = "Float " <> show i
+  show (CharLiteral c) = "Char " <> show c
+  show (StringLiteral s) = "String " <> show s
+  show (ListLiteral xs) = "List " <> show xs
+  show (AtomLiteral a) = "Atom :" <> show a
+  show (ObjectLiteral xs) = "Object " <> show xs
+  show (BinaryLiteral xs) = "Binary " <> show xs
+  show (TupleLiteral a b) = "(" <> show a <> "," <> show b <> ")"
+  show (TupleLiteral3 a b c) = "(" <> show a <> "," <> show b <> "," <> show c <> ")"
+  show (TupleLiteral4 a b c d) = "(" <> show a <> "," <> show b <> "," <> show c <> "," <> show d <> ")"
+  show (TupleLiteral5 a b c d e) = "(" <> show a <> "," <> show b <> "," <> show c <> "," <> show d <> "," <> show e <> ")"
+  show (TupleLiteral6 a b c d e f) = "(" <> show a <> "," <> show b <> "," <> show c <> "," <> show d <> "," <> show e <> "," <> show f <> ")"
+  show (TupleLiteral7 a b c d e f g) = "(" <> show a <> "," <> show b <> "," <> show c <> "," <> show d <> "," <> show e <> "," <> show f <> "," <> show g <> ")"
+
--- tests/Test.purs
+++ tests/Test.purs
@@ -1,19 +1,38 @@
 module Test where
 
-data T = T | N | M | Q
+data T = T | N   -- | M | Q
 
 
-t :: (T,T) -> T
-t (T,_) = T
-t (M,_) = N
-t (x,N) = N
-t (y,Q) = y
-t (N,x) = N
-t (Q,x) = N
+-- t :: (T,T) -> T
+-- t (T,T) = T
+-- t (T,N) = T
+-- t (N,N) = N
+-- t (T,N) = T
+-- t _ = N
+t :: (T,T,T,T,T,T,T) -> T
+t (T,N,N,T,N,T,T) = T
+t (T,T,N,T,N,T,T) = T
+t (T,T,T,T,N,T,T) = T
+t (T,T,T,T,T,T,T) = T
 
-j :: List T -> List T
-j [] = []
-j [T|y] = []
-j [N|y] = []
-j [M|y] = []
-j [Q|y] = []
+
+
+-- k :: (T,T,T,T,T,T)
+-- k =  (T,T,T,T,T,T)
+-- t (N,N,N,N,T,T,N) = T
+-- t (_,_,_,_,_,_,T) = T
+-- t (_,_,T) = T
+
+
+-- t (N,T) = N
+
+-- t (y,Q) = y
+-- t (N,x) = N
+-- t (Q,x) = N
+
+-- j :: List T -> List T
+-- j [] = []
+-- j [T|y] = []
+-- j [N|y] = []
+-- j [M|y] = []
+-- j [Q|y] = []
