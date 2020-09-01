12b8be6d48ae168c4ce0415188001aaed6549496
EMQ-YangM <yangm@emqx.io>
2020-09-01 12:24:48 +0800

    fix(Utils): fix Binary pattern match bug

--- tests/Test.purs
+++ tests/Test.purs
@@ -1,20 +1,5 @@
 module Test where
 
-t = do
-  println "start"
-  receive
-     1 -> println "1"
-     2 -> println "2"
-  after 100 -> println "timeout"
-  v <- receive
-           1 -> return 1
-           2 -> return 2
-       after 100 -> return 3
-  println $ "receive .. " showAny v
-
-k = receive
-      1 -> println "1"
-      2 -> println "2"
-    after 1000 -> println "timeout"
-
+t x = let <<"bab", res/binary>> = x -- <<"babab">>
+    in res
 
