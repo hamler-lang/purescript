c027c77003b41c234721a61a923548271d947441
EMQ-YangM <yangm@emqx.io>
2020-09-03 14:52:50 +0800

    fix(Types): fix "receive .. after" type bug

--- tests/Test.purs
+++ tests/Test.purs
@@ -1,5 +1,24 @@
 module Test where
 
-t x = let <<"bab", res/binary>> = x -- <<"babab">>
-    in res
+foreign import data IO :: Type -> Type
+foreign import return :: [Integer] -> IO [Integer]
+foreign import bind :: forall a b. IO a -> (a -> IO b) -> IO b
+
+-- revFun :: [Integer] -> IO [Integer]
+revFun ys = do
+  -- v <- receive
+  receive
+         y -> revFun [y|ys]
+       after 100 -> return ys
+  -- return [10]
+  -- after 100 -> (return ys :: IO [Integer])
+
+
+
+
+
+
+
+
+
 
