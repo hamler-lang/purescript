c068412621d559c0342e61272247094a59feb0e4
EMQ-YangM <yangm@emqx.io>
2020-09-28 10:36:42 +0800

    feat(Parsery): add new record update syntax

--- tests/Test.purs
+++ tests/Test.purs
@@ -1,24 +1,12 @@
 module Test where
 
-foreign import data IO :: Type -> Type
-foreign import return :: [Integer] -> IO [Integer]
-foreign import bind :: forall a b. IO a -> (a -> IO b) -> IO b
-
--- revFun :: [Integer] -> IO [Integer]
-revFun ys = do
-  -- v <- receive
-  receive
-         y -> revFun [y|ys]
-       after 100 -> return ys
-  -- return [10]
-  -- after 100 -> (return ys :: IO [Integer])
+a = { name="yang"
+    , pos={ x=10, y =20
+          , info= { s ="aa"
+                  , v=45.9
+                  }
+          }
+    }
 
 
-
-
-
-
-
-
-
-
+b = a{pos.info.s = "sdzx"}
